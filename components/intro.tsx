90276245f8b6d324aee8e29d77aba5cf3d36cea9
Rameel Ahmed <Rameel668@hotmail.com>
2023-12-14 16:41:56 +0000

    error

--- components/intro.tsx
+++ components/intro.tsx
@@ -59,7 +59,7 @@ export default function Intro() {
                 initial={{ opacity: 0, y: 100}}
                 animate={{ opacity: 1, y: 0}}
             >
-                <span className='font-bold'>Hello, I&apos;m Rameel</span> I'm a 
+                <span className='font-bold'>Hello, I&apos;m Rameel</span> I&apos;m a 
                 <span className='font-bold'> Full Stack Developer</span> with 
                 <span className='font-bold'> 2 years</span> of experience. I enjoy building 
                 <span className='font-bold'> sites & apps, </span>My focus is 
